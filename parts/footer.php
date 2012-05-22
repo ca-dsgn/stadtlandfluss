230604260705d63b579b36563878bb577f72d2f3
René Preußer <rene.preusser@crenation.de>
2012-05-22 09:56:03 +0200

    footer css processed

--- parts/footer.php
+++ parts/footer.php
@@ -1,6 +1,21 @@
 <footer id="pageFooter">
-	<div class="wrapper">
+	<div id="pageFooterSlider">
+		<div class="wrapper">
+			<div id="pageFooterLeft">asd</div>
+			<div id="pageFooterMiddle">asd</div>
+			<div id="pageFooterRight">asd</div>
+		</div>
+	</div>
+	<div id="pageFooterContent">
+		<div class="wrapper">
+			<ul>
+				<li><a href="">Impressum</a></li>
+				<li><a href="">Datenschutz</a></li>
+				<li><a href="">Bla bla bla</a></li>
+			</ul>
+		</div>
 	</div>
 </footer>
+
 </body>
 </html>

a00ede2d3dbd2fadcceb88b9b3c9bb20204eff7d
Christian Albert <christian-albert@msn.com>
2012-05-22 09:22:19 +0200

    structure

--- parts/header.php
+++ parts/header.php
@@ -2,6 +2,7 @@
 <head>
 	<title>Stadt Land Fluss Geschichten</title>
 	<meta charset="utf-8"/>
+	<link rel="stylesheet" href="css/main.css" type="text/css"/>
 </head>
 <body>
 <header id="pageHeader">

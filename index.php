3518a87e94304655fed4fcc2f6b1015e0c1f8254
Sebastian Lauer <sebastian_lauer@gmx.de>
2012-05-22 15:37:58 +0200

    startseite

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php 
-
 	include 'parts/header.php';
-
 ?>
 
-<div id="content" class="wrapper">
-	<h1>Hier ist der Content-Bereich</h1>
-	<p>asdfasdf asdfasdf asdfasdfasdfasdf asdfasdfasdf asdfasdf.</p>
+<div id="startseite" class="wrapper">
+	
+	<div id="picture">Video</div>
+	<div id="play_button">Play</div>
+	<div id="description">
+		<h1>Max Mustermann</h1>
+		<sub>Motocross-Fahrer</sub>
+		<p>Hier kommt der Beschreibungstext von dem Film mal rein.</p>
+	</div>
+	
+	<div id="arrow_left"><</div>
+	<div id="arrow_right">></div>
 </div>
 
 <?php

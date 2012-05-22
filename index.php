34124663afff407d58cf9afd1a52fe613b0fc2ac
Sebastian Lauer <sebastian_lauer@gmx.de>
2012-05-22 13:11:03 +0200

    Content in index.php

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 
 ?>
 
-
+<div id="content" class="wrapper">
+	<h1>Hier ist der Content-Bereich</h1>
+	<p>asdfasdf asdfasdf asdfasdfasdfasdf asdfasdfasdf asdfasdf.</p>
+</div>
 
 <?php
 	include 'parts/footer.php';

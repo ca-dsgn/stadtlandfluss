94f7d8bac1e8edbb60527c8d03e5d5eaa4847b03
Christian Albert <christian-albert@msn.com>
2012-05-22 13:35:12 +0200

    added database.ph

--- parts/header.php
+++ parts/header.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" href="css/main.css" type="text/css"/>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
     <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+    <script src="http://code.jquery.com/ui/1.8.20/jquery-ui.min.js" type="text/javascript"></script>
 	<script type="text/javascript" src="js/header.js"></script>
 	<script type="text/javascript" src="js/footer.js"></script>
 </head>

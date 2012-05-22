70ecb0ab82e20005be21f0f717d67468d3996310
Christian Albert <christian-albert@msn.com>
2012-05-22 10:51:50 +0200

    added js to header

--- parts/header.php
+++ parts/header.php
@@ -3,6 +3,9 @@
 	<title>Stadt Land Fluss Geschichten</title>
 	<meta charset="utf-8"/>
 	<link rel="stylesheet" href="css/main.css" type="text/css"/>
+	<script type="text/javascript" src="js/header.js"></script>
+	<script type="text/javascript" src="js/footer.js"></script>
+
 </head>
 <body>
 <header id="pageHeader">

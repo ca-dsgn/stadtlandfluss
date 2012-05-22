3e0e3c79407cf91292de7c12fec87f8a0ef32e21
Christian Albert <christian-albert@msn.com>
2012-05-22 11:12:14 +0200

    added jquery easing

--- parts/header.php
+++ parts/header.php
@@ -4,9 +4,9 @@
 	<meta charset="utf-8"/>
 	<link rel="stylesheet" href="css/main.css" type="text/css"/>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
 	<script type="text/javascript" src="js/header.js"></script>
 	<script type="text/javascript" src="js/footer.js"></script>
-
 </head>
 <body>
 <header id="pageHeader">

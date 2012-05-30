e151dacc1288cac053c4e768a9e0e62ce7f85085
René Preußer <rene.preusser@crenation.de>
2012-05-30 19:32:43 +0200

    footer edited

--- parts/footer.php
+++ parts/footer.php
@@ -1,37 +1,36 @@
-<footer id="pageFooter">
-	<div id="pageFooterSlider">
-		<div id="pageFooterSliderButton"></div>
-		<div id="pageFooterSliderContent">
-			<div class="wrapper">
-				<div id="pageFooterLeft">
-					<h2>Kennenlernen</h2>
-					<p>AFFE</p>
-				</div>
-				<div id="pageFooterMiddle">
-					<h2>Mitmachen</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisici elit. Lorem ipsum dolor sit amet, consectetur adipisici elit. Lorem ipsum dolor sit amet, consectetur adipisici elit. Lorem ipsum dolor sit amet, consectetur adipisici elit. Lorem ipsum dolor sit amet, consectetur adipisici elit.</p>
-				</div>
-				<div id="pageFooterRight">
-					<h2>Empfehlen</h2>
-					<p>AFFE</p>
+		<!-- PAGE SLIDER-BOX [BEGIN] -->
+		<div id="pageFooterSlider">
+			<div id="pageFooterSliderButton"></div>
+			<div id="pageFooterSliderContent">
+				<div class="wrapper">
+					<div id="pageFooterLeft">
+						<h2>Kennenlernen</h2>
+						<p>AFFE</p>
+					</div>
+					<div id="pageFooterMiddle">
+						<h2>Mitmachen</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisici elit. Lorem ipsum dolor sit amet, consectetur adipisici elit. Lorem ipsum dolor sit amet, consectetur adipisici elit. Lorem ipsum dolor sit amet, consectetur adipisici elit. Lorem ipsum dolor sit amet, consectetur adipisici elit.</p>
+					</div>
+					<div id="pageFooterRight">
+						<h2>Empfehlen</h2>
+						<p>AFFE</p>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div id="pageFooterContent">
-
-		<p>© 2012 DHBW Mosbach – All Rights reserved</p>
-
+		<!-- PAGE SLIDER-BOX [END] -->
+	
+		<!-- PAGE FOOTER [BEGIN] -->
+		<footer id="pageFooter">
+			<p>© 2012 <a href="http://www.dhbw-mosbach.de/" target="_blank">DHBW Mosbach</a> – All Rights reserved</p>
 			<ul id="footerNav">
 				<li><a href="">Kontakt</a></li>
 				<li><a href="">Datenschutz</a></li>
 				<li><a href="">Impressum</a></li>
 				<li class="overProject"><a href="">Über das Projekt</a></li>
 			</ul>
-			
-			
-	</div>
-</footer>
+		</footer>
+		<!-- PAGE FOOTER [END] -->
 
 </body>
 </html>

6bca87b3d6100a6a0cc610e037311931ee35884d
Christian Albert <christian-albert@msn.com>
2012-06-05 10:43:43 +0200

    slidehow moveable for touch devices

--- js/header.js
+++ js/header.js
@@ -10,16 +10,17 @@ $(document).ready(function() {
 	});
 	$(".arrowRight").live("click",function() {
 		
-		
+		slideshow_move("right");
 	});
 	$(".arrowLeft").live("click",function() {
 		
-		
+		slideshow_move("left");
 	});
 	slideShowPositioning();
 	$(window).resize(function() {
 		slideShowPositioning();
 	});
+	slidehowDraggable();
 });
 
 function blow_play_automat(on_or_off) {
@@ -77,9 +78,81 @@ function slideshow_move(direction) {
 		
 		case "left":
 		
+			$(".contentBox.following").animate({
+				left: "-=1280",
+				opacity: 1
+			});
+			$(".contentBox.current").animate({
+				left: "-=1280",
+				opacity: 0
+			});
+			$(".contentBox.previous").remove();
+			
+			$(".contentBox.current").addClass("previous");
+			$(".contentBox.current").removeClass("current");
+			
+			$(".contentBox.following").addClass("current");
+			$(".contentBox.following").removeClass("following");
+			
 			break;
 		case "right":
 		
+			$(".contentBox.previous").animate({
+				left: "+=1280",
+				opacity: 1
+			});
+			$(".contentBox.current").animate({
+				left: "+=1280",
+				opacity: 0
+			});
+			$(".contentBox.following").remove();
+			
+			$(".contentBox.current").addClass("following");
+			$(".contentBox.current").removeClass("current");
+			
+			$(".contentBox.previous").addClass("current");
+			$(".contentBox.previous").removeClass("previous");
 			break;
 	}
+}
+
+function slidehowDraggable() {
+
+	$(".contentBox").draggable({
+		
+		axis: "x",
+		stop: function(event,ui) {
+			
+			if (($(window).width()-$(event.target).width())/2 - ui.offset.left > 0) {
+				
+				if (($(window).width()-$(event.target).width())/2 - ui.offset.left > 50) {
+					
+					//WANTS TO GO LEFT
+					slideshow_move("left");
+				}
+				else {
+					
+					$(".contentBox.current").animate({
+						
+						left: "0"
+					}, 300);
+				}
+			}
+			if (($(window).width()-$(event.target).width())/2 - ui.offset.left < 0) {
+				
+				if (($(window).width()-$(event.target).width())/2 - ui.offset.left < -50) {
+					
+					//WANTS TO GO RIGHT
+					slideshow_move("right");
+				}
+				else {
+					
+					$(".contentBox.current").animate({
+						
+						left: "0"
+					}, 300);
+				}
+			}
+		}
+	});	
 }

e288539833be10fc2856906961b082d771ef50fc
Rene Preisler <keep82@gmx.de>
2012-06-13 14:05:46 +0200

    Test Class changed

--- php/TestClass.php
+++ php/TestClass.php
@@ -1,23 +1,24 @@
 <?php
 include_once ("VideoController.php");
+echo ("GetVideo:<br/>");
 $vc = new VideoController();
 echo $vc->getVideo(2);
-echo ("<br/>");
+echo ("<br/><br/>PreviousVideo:<br/>");
 echo $vc->getPreviousVideo(2);
-echo ("<br/>");
+echo ("<br/><br/>NextVideo:<br/>");
 echo $vc->getNextVideo(2);
-echo ("<br/>");
+echo ("<br/><br/>Tags:<br/>");
 echo $vc->getTags(2);
-echo ("<br/>");
+echo ("<br/><br/>Bilder:<br/>");
 echo $vc->getImages(2);
-echo ("<br/>");
+echo ("<br/><br/>Kommentare:<br/>");
 echo $vc->getComments(2);
-echo ("<br/>");
+echo ("<br/><br/>Personen:<br/>");
 echo $vc->getPersons(0);
-echo ("<br/>");
+echo ("<br/><br/>Rückgabe der MatrixView Videos:<br/>");
 echo $vc->getMatrixView(0,5);
-echo ("<br/>");
+echo ("<br/><br/>Anzahl der eingepflegten Videos:<br/>");
 echo $vc->getNumOfVideos();
-echo ("<br/>");
+echo ("<br/><br/>All Locations:<br/>");
 echo $vc->getAllLocations();
 ?>

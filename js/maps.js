4ca9495d07a17be793132e1f321133f341c2b578
Christian Albert <christian-albert@msn.com>
2012-06-14 14:02:28 +0200

    corrected maps javascript warnings

--- js/maps.js
+++ js/maps.js
@@ -6,29 +6,32 @@ var markersArray = [];
 
 function initialize() {
 	
-	directionsDisplay = new google.maps.DirectionsRenderer();
-	var allekotte = new google.maps.LatLng(50.542296,7.241529);
-	
-	var myOptions = {
-		zoom: 16,
-		center: allekotte,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-	
-	var marker = new google.maps.Marker({
-	  position: allekotte, 
-	  map: map, 
-	  title:"Bla"
-	});
-	
-	google.maps.event.addListener(marker, 'click', function() {
+	if (document.getElementById("map_canvas") != null) {
 		
-		alert("TEST");
-	});
-	
-	markersArray.push(marker);
-	directionsDisplay.setMap(map);
+		directionsDisplay = new google.maps.DirectionsRenderer();
+		var allekotte = new google.maps.LatLng(50.542296,7.241529);
+		
+		var myOptions = {
+			zoom: 16,
+			center: allekotte,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+		
+		var marker = new google.maps.Marker({
+		  position: allekotte, 
+		  map: map, 
+		  title:"Bla"
+		});
+		
+		google.maps.event.addListener(marker, 'click', function() {
+			
+			alert("TEST");
+		});
+		
+		markersArray.push(marker);
+		directionsDisplay.setMap(map);
+	}
 }
 
 function removeMarkers() {

eb7f31fc50c716831158cc8437bcf8f6160916fe
heartcrazed <katharina.franz@gmail.com>
2012-06-14 21:40:36 +0200

    added tierheim

--- portrait.php
+++ portrait.php
@@ -8,7 +8,7 @@
 		
 		<ul class="content">
 			<li class="previous contentBox">
-				<div class="picture">Video</div>
+				<div class="picture"></div>
 				<div class="playButton"></div>
 				<div class="description">
 				<hgroup>
@@ -33,14 +33,14 @@
 			</li>
 			
 			<li class="following contentBox">
-				<div class="picture">Video</div>
+				<div class="picture"><img src="img/portraits/tierheim2.png" /></div>
 				<div class="playButton"></div>
 				<div class="description">
 				<hgroup>
-					<h1>Uli Körber</h1>
-					<h2>Motocross-Fahrer</h2>
+					<h2>Brigitte Schmitt</h2>
+					<h1>Tierheim Dallau – Jeder Käfig hat seine Geschichte</h1>
 				</hgroup>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+					<p>Mit einem besonders großen Einzugsgebiet im Neckar-Odenwald-Kreis bietet das Tierheim Dallau vielen Tieren ein temporäres Zuhause – manchen länger, anderen nur kurz. Wir haben Brigitte Schmitt, eines der langjährigen Mitglieder des Vereins begleitet, wie sie um das Schicksal des Schäferhundrüden „Sultan“ bangt, der fünf Jahre lang nicht vermittelt werden konnte.</p>
 				</div>
 			</li>
 			

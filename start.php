9f8b06860aac64058f8cef8a2927debe336d7d29
René Preußer <rene.preusser@crenation.de>
2012-06-14 16:13:01 +0200

    asd

--- start.php
+++ start.php
@@ -1,9 +1,8 @@
 <div id="startView">
 	
 <video loop="" autoplay="" tabindex="0" style="width: 100%; height: 100%">
-<source type="video/mp4" src="http://www.blacknegative.com/uploads/section/whoweare/loop.mp4"></source>
-<source type="video/webm" src="http://www.blacknegative.com/uploads/section/whoweare/loop.webm"></source>
-<source type="video/ogg" src="http://www.blacknegative.com/uploads/section/whoweare/loop.ogv"></source>
+<source type="video/mp4" src="01.mp4"></source>
+
 </video>
 	
 </div>

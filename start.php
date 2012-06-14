a591e3bc0c0ecdffdc1a78f59de3f9184b638e5d
Christian Albert <christian-albert@msn.com>
2012-06-14 11:43:39 +0200

    video size is much better now

--- start.php
+++ start.php
@@ -1,6 +1,6 @@
 <div id="startView">
 	
-	<video loop="" autoplay="" tabindex="0" style="width: 1280px; height: 720px; left: 0px; top: -154.5px;">
+<video loop="" autoplay="" tabindex="0" style="width: 100%; height: 100%">
 <source type="video/mp4" src="http://www.blacknegative.com/uploads/section/whoweare/loop.mp4"></source>
 <source type="video/webm" src="http://www.blacknegative.com/uploads/section/whoweare/loop.webm"></source>
 <source type="video/ogg" src="http://www.blacknegative.com/uploads/section/whoweare/loop.ogv"></source>

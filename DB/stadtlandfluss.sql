0252ea7fd8714c41731c0f1c9e1a4d6295f05f41
Rene Preisler <keep82@gmx.de>
2012-06-15 13:27:52 +0200

    neue Daten in DB

--- DB/stadtlandfluss.sql
+++ DB/stadtlandfluss.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 15. Jun 2012 um 13:07
+-- Erstellungszeit: 15. Jun 2012 um 13:27
 -- Server Version: 5.5.16
 -- PHP-Version: 5.3.8
 
@@ -80,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `person2video` (
   `Video_ID` int(5) DEFAULT NULL,
   `Person_ID` int(5) DEFAULT NULL,
   PRIMARY KEY (`p2v_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- Daten für Tabelle `person2video`
@@ -92,7 +92,9 @@ INSERT INTO `person2video` (`p2v_ID`, `Video_ID`, `Person_ID`) VALUES
 (2, 0, 2),
 (3, 1, 1),
 (4, 1, 2),
-(5, 2, 0);
+(5, 2, 0),
+(6, 3, 4),
+(7, 4, 3);
 
 -- --------------------------------------------------------
 
@@ -106,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `persons` (
   `function` varchar(255) DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`Person_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Daten für Tabelle `persons`
@@ -115,7 +117,9 @@ CREATE TABLE IF NOT EXISTS `persons` (
 INSERT INTO `persons` (`Person_ID`, `name`, `function`, `type`) VALUES
 (0, 'Alex', 'kamera', 'dozent'),
 (1, 'Daniela', 'Schnitt', 'dozent'),
-(2, 'Christoph', 'Aufnahmeleiter', 'student');
+(2, 'Christoph', 'Aufnahmeleiter', 'student'),
+(3, 'Brigitte Schmitt', 'Tierheim Dallau', 'Protagonist'),
+(4, 'Werner Simon', 'Polizei Mosbach', 'Protagonist');
 
 -- --------------------------------------------------------
 
@@ -176,7 +180,7 @@ CREATE TABLE IF NOT EXISTS `videos` (
   `longitude` varchar(30) DEFAULT NULL,
   `altitude` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`Video_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Daten für Tabelle `videos`
@@ -186,7 +190,8 @@ INSERT INTO `videos` (`Video_ID`, `date`, `title`, `subtitle`, `source`, `descri
 (0, '2012-06-12 13:05:11', 'Trailer StadtLandFluss-Geschichten', 'subtitle aus DB', 'http://www.youtube.com/watch?v=zYXjLbMZFmo', 'Seit Jahren entstehen am Studiengang Onlinemedien an der DHBW Mosbach kurze Portraits von Personen rund um Mosbach. Die Filmemacher Daniela Michel und Alexander Kleider leiten das jährlich stattfindende Dokumentarfilmseminar. Der Trailer zum darauf basierenden Projekt StadtLandFluss-Geschichten (www.stadtlandfluss-geschichten.de), das von Prof. Dr. Thomas Wirth initiiert wurde, schaut den Studenten bei der Filmarbeit über die Schulter und lässt Dozenten und Studenten zu Wort kommen.', 'http://www.backgroundlabs.com/twitter/4.jpg', 'http://www.dorsch.com/_images/_coolhunting_images_puma-Bike-Profile.jpg', '9.148206', '49.3515'),
 (1, '2012-05-22 02:02:44', 'Santa Farina – Das Göttlichste, was man sich erlauben kann', 'subtitle aus DB', 'http://www.youtube.com/watch?v=NSFIv7HgNdk', 'Sigfried Raether, von allen Siggi genannt, ist leidenschaftlicher Pizzabäcker. Seine Pizzeria „Santa Farina“ befindet sich in der Nähe der S-Bahnhaltstellte Mosbach (Baden). Siggi ist durch seine charmante italienische Art stadtbekannt. Durch seine ganz besonderen Techniken die Zutaten auf die Pizza zu bringen, ist es schon ein Highlight ihm beim Backen zuzuschauen. Mehrere Studierende von ON09 sind regelmäßig in der Mittagspause zu Gast bei Siggi und wollten mehr über diese außergewöhnliche Persönlichkeit, die der Pizzeria einen heimeligen Flair verleiht, erfahren.', 'http://www.backgroundlabs.com/twitter/4.jpg', 'http://www.dorsch.com/_images/_coolhunting_images_puma-Bike-Profile.jpg', '9.144452', '49.351591'),
 (2, '2012-05-22 01:57:27', 'Benzin im Blut - Zwischen Motocross und CorelDraw', 'subtitle aus DB ', 'http://www.youtube.com/watch?v=3ZdMAk9aqU0&feature=g-upl', 'Uli Körber ist selbstständiger Grafikdesigner in Schefflenz. Doch das ist nicht alles. Uli hat Benzin im Blut! Um einen Ausgleich zu seinem Beruf zu haben, geht er regelmäßig MotoCross fahren. Mit Hilfe dieses außergewöhnlichen Hobbys und dessen Preisgeldern konnte er sein Studium finanzieren. Uli wurde sogar Deutscher- und Europameister! Sabine Bennebach von der Bürgerstiftung für die Region Mosbach gab uns den Tipp zu diesem Portrait.', 'http://www.backgroundlabs.com/twitter/4.jpg', 'http://www.dorsch.com/_images/_coolhunting_images_puma-Bike-Profile.jpg', '9.254093', '49.394022'),
-(3, '2012-06-15 13:01:05', 'Tierheim Dallau - Jeder Käfig hat seine Geschichte', 'subtitle aus DB', 'http://www.youtube.com/watch?v=NSFIv7HgNdk', 'Mit einem besonders großen Einzugsgebiet im Neckar-Odenwald-Kreis bietet das Tierheim Dallau vielen Tieren ein temporäres Zuhause - manchen länger, anderen nur kurz. Wir haben Brigitte Schmitt, eines der langjährigen Mitglieder des Vereins begleitet, wie sie um das Schicksal des Schäferhundrüden „Sultan" bangt, der fünf Jahre lang nicht vermittelt werden konnte. Florian Eberle und Matthias Weise (Studierende des Jahrgangs 2009) wollten eine Tierheimgeschichte erzählen und sind so auf das Mosbacher Tierheim in Dallau gestoßen.', 'http://www.backgroundlabs.com/twitter/4.jpg', 'http://www.dorsch.com/_images/_coolhunting_images_puma-Bike-Profile.jpg', '9.2004', '49.384641');
+(3, '2012-06-15 13:01:05', 'Tierheim Dallau - Jeder Käfig hat seine Geschichte', 'subtitle aus DB', 'http://www.youtube.com/watch?v=NSFIv7HgNdk', 'Mit einem besonders großen Einzugsgebiet im Neckar-Odenwald-Kreis bietet das Tierheim Dallau vielen Tieren ein temporäres Zuhause - manchen länger, anderen nur kurz. Wir haben Brigitte Schmitt, eines der langjährigen Mitglieder des Vereins begleitet, wie sie um das Schicksal des Schäferhundrüden „Sultan" bangt, der fünf Jahre lang nicht vermittelt werden konnte. Florian Eberle und Matthias Weise (Studierende des Jahrgangs 2009) wollten eine Tierheimgeschichte erzählen und sind so auf das Mosbacher Tierheim in Dallau gestoßen.', 'http://www.backgroundlabs.com/twitter/4.jpg', 'http://www.dorsch.com/_images/_coolhunting_images_puma-Bike-Profile.jpg', '9.2004', '49.384641'),
+(4, '2012-06-15 13:13:57', 'Vorsicht, Kontrolle!', 'subtitle aus DB', 'http://www.youtube.com/watch?v=NSFIv7HgNdk', 'Werner Simon ist stellvertretender Dienstgruppenführer der Verkehrspolizei Mosbach und sorgt mit der Überwachung des Schwerverkehrs für mehr Sicherheit auf den Straßen in und um Mosbach. Der Kontakt kam über Lisa Simon (ON09) zustande.', 'http://www.backgroundlabs.com/twitter/4.jpg', 'http://www.dorsch.com/_images/_coolhunting_images_puma-Bike-Profile.jpg', '9.146783', '49.355092');
 
 -- --------------------------------------------------------
 

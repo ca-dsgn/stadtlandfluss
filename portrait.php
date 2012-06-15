bd286eaf807a9c1e058f0bd0d5b73cfc546a53d9
René Preußer <rene.preusser@crenation.de>
2012-06-15 11:31:33 +0200

    asdasdads

--- portrait.php
+++ portrait.php
@@ -1,24 +1,23 @@
 <div id="protagonistContent">
 	<div class="wrapper">
-	
-		<!-- <div class="content">
-
-		</div> -->
-		
 		
 		<ul class="content">
 			<li class="previous contentBox">
-				<div class="picture"></div>
+				<img src="img/portraits/motocross.png" class="picture" />
 				<div class="playButton"></div>
 				<div class="description">
-				<hgroup>
-					<h1>Siegfried Raether</h1>
-					<h2>Pizzabäcker bei »Santa Farina« in Mosbach</h2>
-				</hgroup>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+					<hgroup>
+						<h2>Uli Körber</h2>
+						<h1>Benzin im Blut – Zwischen Motocross und Corel Draw</h1>
+					</hgroup>
+					<p>
+						<a href="" class="infoLink">weitere Infos</a>
+						<a href="" class="playlistLink">zur Playlist hinzufügen</a>
+						<a href="" class="commentLink">Kommentare</a>
+					</p>
 				</div>
 			</li>
-			
+
 			<li class="current contentBox">
 				<img src="img/portraits/motocross.png" class="picture" />
 				<div class="playButton"></div>
@@ -50,14 +49,10 @@
 					</p>
 				</div>
 			</li>
-			
 		</ul>
-		
-
 
 		<div class="arrowLeft"></div>
 		<div class="arrowRight"></div>
-
 
 	</div>
 </div>

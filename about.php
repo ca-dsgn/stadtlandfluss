6b4bdc20daac854478b110a064751d1500649af0
heartcrazed <katharina.franz@gmail.com>
2012-06-16 17:01:05 +0200

    css aufgeräumt

--- about.php
+++ about.php
@@ -30,17 +30,15 @@
 
 <div id="content">
 	<div class="wrapper">
-		<div style="margin: 0 auto; width: 25%; max-width: 960px; min-width: 720px;">
+		<div id="about">
 		
 		<!-- YOUTUBE VIDEO CODE -->
-		
-		<iframe width="720" height="405" src="http://www.youtube.com/embed/qqLaRYxJeno?rel=0" frameborder="0" allowfullscreen style="margin-top: 100px;"></iframe>
-		
+		<iframe id="trailer" width="720" height="405" src="http://www.youtube.com/embed/qqLaRYxJeno?rel=0" frameborder="0" allowfullscreen></iframe>
 		<!-- END YOUTUBE VIDEO CODE -->
 		
-		<h2 style="margin: 30px 0 30px 0; background-color: rgba(96,206,240,0.6); padding: 20px; -moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius:10px;">„Wie oft kommt es vor, dass ein Mensch eine interessante Facette hat, die niemand oder nur sehr wenige kennen?“</h2>
+		<h2 class="zitat">„Wie oft kommt es vor, dass ein Mensch eine interessante Facette hat, die niemand oder nur sehr wenige kennen?“</h2>
 		
-		<p style="margin-bottom: 100px;">Das Projekt <strong>StadtLandFluss-Geschichten</strong> will diese durch kleine Dokumentarfilme, welche seit 2004 vom <a href="http://dhbw-mosbach.de/on" target="_blank">Studiengang Onlinemedien</a> (früher: Digitale Medien) an der <a href="http://www.dhbw-mosbach.de" target="_blank">Dualen Hochschule Mosbach</a> in Kooperation mit der DOKWERK Filmkooperative produziert werden, zum Vorschein bringen und auf einer Projektwebseite zusammentragen. Mit dem Projekt sollen die Voraussetzungen geschaffen werden, die bisher relativ lose nebeneinander stehenden Filmclips zu einer Art „Gesamtkunstwerk“ zusammenzusetzen. In diesem Zusammenhang ist die Zeitperspektive wichtig: Es sollen über Jahre hinweg weitere Erzählungen gesammelt und dem Projekt dann immer weiter hinzugefügt werden.</p>
+		<p id="last">Das Projekt <strong>StadtLandFluss-Geschichten</strong> will diese durch kleine Dokumentarfilme, welche seit 2004 vom <a href="http://dhbw-mosbach.de/on" target="_blank">Studiengang Onlinemedien</a> (früher: Digitale Medien) an der <a href="http://www.dhbw-mosbach.de" target="_blank">Dualen Hochschule Mosbach</a> in Kooperation mit der DOKWERK Filmkooperative produziert werden, zum Vorschein bringen und auf einer Projektwebseite zusammentragen. Mit dem Projekt sollen die Voraussetzungen geschaffen werden, die bisher relativ lose nebeneinander stehenden Filmclips zu einer Art „Gesamtkunstwerk“ zusammenzusetzen. In diesem Zusammenhang ist die Zeitperspektive wichtig: Es sollen über Jahre hinweg weitere Erzählungen gesammelt und dem Projekt dann immer weiter hinzugefügt werden.</p>
 		</div>
 	</div>
 </div>

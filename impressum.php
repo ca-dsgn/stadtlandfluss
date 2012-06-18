e9f3581a7b4309ac57a889d0ff37e0c5673371f0
heartcrazed <katharina.franz@gmail.com>
2012-06-18 14:15:08 +0200

    inner scroll div

--- impressum.php
+++ impressum.php
@@ -1,34 +1,8 @@
-﻿<!DOCTYPE html>
-<html>
-    <head>
-        <title>Impressum – StadtLandFluss-Geschichten</title>
-        <meta charset="utf-8"/>
-        <link rel="stylesheet" href="css/main.css" type="text/css"/>
-        <link rel="shortcut icon" href="./favicon.png" type="image/png"/>
-        <link rel="icon" href="./favicon.png" type="image/png"/>
-        <script src="js/jquery-1.7.2.min.js"></script>
-        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-        <script src="http://code.jquery.com/ui/1.8.20/jquery-ui.min.js" type="text/javascript"></script>
-        <script src="js/jquery.ui.touch-punch.min.js" type="text/javascript"></script>
-        <script type="text/javascript" src="js/timer.js"></script>
-        <script type="text/javascript" src="js/header.js"></script>
-        <script type="text/javascript" src="js/main.js"></script>
-        <script type="text/javascript" src="js/footer.js"></script>
-        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-        <script type="text/javascript" src="js/maps.js"></script>
-    </head>
-    <body>
-    <header id="pageHeader">
-        <div class="wrapper">
-            <a href="./" class="logoBig">StadtLandFluss-Geschichten</a>
-        </div>
-    </header>
+﻿<?php 
+	include "parts/header.php";
+?>
 
-
-
-<!-- END HEAD -->
-
-<div id="content">
+<div id="singlepageContent">
 	<div class="wrapper">
 		<div id="text">
 		

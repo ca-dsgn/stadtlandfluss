c14b33be0987a0943942d6f00b50732bc2f07e67
René Preußer <rene.preusser@crenation.de>
2012-06-19 10:42:32 +0200

    detail-view

--- detail.php
+++ detail.php
@@ -1,13 +1,38 @@
 <div id="detailContent">
 	<div class="wrapper">
 		
-
-
-		<div id="detailVideoPlayer">asd</div>
+		<div id="detailVideoPlayer">
+			<video loop="" autoplay="" tabindex="0">
+				<source type="video/mp4" src="01.mp4"></source>
+				<source type="video/webm" src="01.webm"></source>
+			</video>	
+		</div>
 		
-		<div id="detailLocationBox">asd</div>
+		<div id="movieDetailBox">
+			<h2>Film-Informationen</h2>
+			<h3>Protagonist:</h3>
+			<p><span>Uli Körber</span></p>
+			<h3>Wo war das?:</h3>
+			<div id="movieDetailMap"></div>
+			<h3>ein Film von:</h3>
+			<p><span>Elina Wetsch, Lisa Simon, Christian Albert, Sebastian Lauer, René Preußer</span></p>
+
+		</div>
 		
-		<div id="detailCommentBox">asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br>asd</br></div>
+		<div id="detailCommentBox"><div id="disqus_thread"></div>
+        <script type="text/javascript">
+            /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+            var disqus_shortname = 'stadtlandflussgeschichten'; // required: replace example with your forum shortname
+
+            /* * * DON'T EDIT BELOW THIS LINE * * */
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a></div>
 		
 		
 	</div>

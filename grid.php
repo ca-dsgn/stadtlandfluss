91c3f34e0e1b8f0335b21dbd6ff0e41cb226c3ff
Christian Albert <christian-albert@msn.com>
2012-06-19 12:33:02 +0200

    now there a external helper class

--- grid.php
+++ grid.php
@@ -3,46 +3,7 @@
 			
 	$vc = new VideoController();
 	
-	function getVideoTemplateById($id) {
-								
-		global $vc;
-		
-		$element = json_decode($vc->getVideoWithImages($id));
-		
-		return getTemplate($element[0]->Video_ID,$element[0]->title,$element[0]->description,$element[0]->images);
-	}
 	
-	function getVideoTemplate($id,$title,$description,$images) {
-		
-		return getTemplate($id,$title,$description,$images);
-	}
-	
-	function getTemplate($id,$title,$description,$images) {
-		
-		$html = '<li class="item" ref="'.$id.'">';
-		$html.= '<div class="box">';
-		$html.= '<img src="'.$images[0]->url.'" alt="'.$images[0]->alt.'"/>';
-		$html.= '</div>';
-		$html.= '<div class="images">';
-		$html.= '<div class="top">';
-		$html.= '<img src="'.$images[1]->url.'" alt="'.$images[1]->alt.'"/>';
-		$html.= '</div>';
-		$html.= '<div class="bottom">';
-		$html.= '<img src="'.$images[2]->url.'" alt="'.$images[2]->alt.'"/>';
-		$html.= '</div>';
-		$html.= '</div>';
-		$html.= '<div class="info">';
-		$html.= '<div class="info_box">';
-		$html.= '<h2>'.$title.'</h2>';
-		$html.= '<p>'.$description.'</p>';
-		$html.= '<a>Link</a>';
-		$html.= '<a>Link</a>';
-		$html.= '</div>';
-		$html.= '</div>';
-		$html.= '</li>';
-		
-		return $html;
-	}
 ?>
 
 <div id="gridContent">

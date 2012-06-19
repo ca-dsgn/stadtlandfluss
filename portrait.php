304395d0ba752e423933b099c427fb45e71aef4b
René Preußer <rene.preusser@crenation.de>
2012-06-19 18:07:49 +0200

    asdad

--- portrait.php
+++ portrait.php
@@ -1,11 +1,12 @@
 <div id="protagonistContent">
 	<div class="backgroundImages">
-    	<div class="backgroundImage"></div>
-        <div class="backgroundImage"></div>
-        <div class="backgroundImage"></div>
-        <div class="backgroundImage"></div>
-        <div class="backgroundImage"></div>
-    </div>
+		<div class="backgroundImage" id="backWinsun"></div>
+    <div class="backgroundImage" id="backModerne"></div>
+		<div class="backgroundImage" id="backWind"></div>
+
+    <div class="backgroundImage"></div>
+    <div class="backgroundImage"></div>
+  </div>
 	<div class="wrapper">
 		<ul>
 			<li class="contentBox">
@@ -24,11 +25,12 @@
 			</li>
 
 			<li class="contentBox">
+				<img src="img/backgrounds/moderne-single.jpg" class="portraitIMG" alt="moderne-single" width="780" height="780" />
 				<div class="playButton"></div>
 				<div class="description">
 					<hgroup>
-						<h2>Uli Körber</h2>
-						<h1>Benzin im Blut – Zwischen Motocross und Corel Draw</h1>
+						<h2>Baron Dajo von Gemmingen-Hornberg</h2>
+						<h1>Zwischen Tradition und Moderne</h1>
 					</hgroup>
 					<p>
 						<a href="" class="infoLink">weitere Infos</a>

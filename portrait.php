7e58613083d3c8fc8dc7481ade5448e035633efb
René Preußer <rene.preusser@crenation.de>
2012-06-20 14:44:21 +0200

    asd

--- portrait.php
+++ portrait.php
@@ -42,7 +42,6 @@
 				$html.= '</hgroup>';
 				$html.= '<p>';
 				$html.= '<a href="index.php?section=detail&video_id='.$element[0]->Video_ID.'" class="infoLink">weitere Infos</a>';
-				$html.= '<a href="" class="playlistLink" alt="Zur Playlist hinzufügen">zur Playlist hinzufügen</a>';
 				$html.= '</p>';
 				$html.= '</div>';
 				$html.= '</li>';

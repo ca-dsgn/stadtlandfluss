0d90a3b51d41e5b0bbcf4d547c7bbc9de870eb81
heartcrazed <katharina.franz@gmail.com>
2012-07-03 14:16:33 +0200

    z-index problem durch verschieben des schließenbuttons gelöst

--- about.php
+++ about.php
@@ -10,28 +10,29 @@
 		<iframe id="trailer" width="720" height="405" src="http://www.youtube.com/embed/qqLaRYxJeno?rel=0" frameborder="0" allowfullscreen></iframe>
 		<!-- END YOUTUBE VIDEO CODE -->
 		
-		<blockquote class="zitat">„Wie oft kommt es vor, dass ein Mensch eine interessante Facette hat, die niemand oder nur sehr wenige kennen?“</blockquote>
+		<blockquote class="zitat">&bdquo;Wie oft kommt es vor, dass ein Mensch eine interessante Facette hat, die niemand oder nur sehr wenige kennen?&ldquo;</blockquote>
 		
 		<h2>Die Entstehungsgeschichte</h2>
-<p>Das Projekt "StadtLandFluss-Geschichten" beruht auf einer Kooperation des Studiengangs Onlinemedien an der DHBW Mosbach und den Dokumentarfilmern der DOK-WERK Filmkooperative aus Köln. Diese Akteure stehen für die beiden wesentlichen Bestandteile des Projekts: Web-Technologie und Web-Design einerseits, und Film bzw. Dokumentarfilm andererseits. Das Projekt bildet einen gemeinsamen konzeptionellen Bezugsrahmen für die in der Lehre an der Hochschule im Rahmen des Curriculums "Audiovisuelle Gestaltung" immer wieder neu entstehenden Dokumentarfilme. Diese sollen nicht einfach gesammelt und archivert, sondern innerhalb eines wachsenden "Online-Filmwerks" zusammengesetzt und veröffentlicht werden.</p>
+		<p>Das Projekt &bdquo;StadtLandFluss-Geschichten&ldquo; beruht auf einer Kooperation des <a href="http://www.dhbw-mosbach.de/on">Studiengangs Onlinemedien an der DHBW Mosbach</a> und den Dokumentarfilmern der <a href="http://www.dok-werk.com">DOK-WERK Filmkooperative</a> aus K&ouml;ln. Diese Akteure stehen f&uuml;r die beiden wesentlichen Bestandteile des Projekts: Web-Technologie und Web-Design einerseits, und Film bzw. Dokumentarfilm andererseits. Das Projekt bildet einen gemeinsamen konzeptionellen Bezugsrahmen f&uuml;r die in der Lehre an der Hochschule im Rahmen des Curriculums &bdquo;Audiovisuelle Gestaltung&ldquo; immer wieder neu entstehenden Dokumentarfilme. Diese sollen nicht einfach gesammelt und archiviert, sondern innerhalb eines wachsenden &bdquo;Online-Filmwerks&ldquo; zusammengesetzt und ver&ouml;ffentlicht werden.</p>
 
-<h2>Zielsetzung des Projekts innerhalb der Lehre</h2>
-<p>Die StadtLandFluss-Geschichten sollen es ermöglichen, in Zukunft auch mit den sich aktuell rasch entwickelnden technischen Möglichkeiten zu experimentieren, die das Web für die Gestaltung filmischer Erzählungen zur Verfügung stellt. Derzeit schon realisiert ist die eine interaktive Landkarte, auf der Episoden nach ihrer geografischen Ordnung angeordnet, gefunden und ausgewählt werden können und die Entwicklung einer grafischen Benutzeroberfläche für das Zusammenstellen von "Playlists". Komplexere Beispiele, die in Zukunft realisert werden sollen, könnten z.B. das Einblenden von Information außerhalb des primären Video-Streams zu bestimmten Zeitpunkten oder auch das interaktive Navigieren im Filmmaterial selbst betreffen. Obwohl derzeit noch niemand genau weiß, was die Zukunft hier bringen wird, möchten wir bei dieser Entwicklung dabei sein.</p>
+		<h2>Zielsetzung des Projekts innerhalb der Lehre</h2>
+		<p>Die StadtLandFluss-Geschichten sollen es erm&ouml;glichen, in Zukunft auch mit den sich aktuell rasch entwickelnden technischen M&ouml;glichkeiten zu experimentieren, die das Web f&uuml;r die Gestaltung filmischer Erz&auml;hlungen zur Verf&uuml;gung stellt. Derzeit schon realisiert ist die eine interaktive <a href="./index.php?section=map">Landkarte</a>, auf der Episoden nach ihrer geografischen Ordnung angeordnet, gefunden und ausgew&auml;hlt werden k&ouml;nnen und die Entwicklung einer grafischen Benutzeroberfl&auml;che f&uuml;r das Zusammenstellen von &bdquo;Playlists&ldquo;. Komplexere Beispiele, die in Zukunft realisert werden sollen, k&ouml;nnten z.B. das Einblenden von Information au&szlig;erhalb des prim&auml;ren Video-Streams zu bestimmten Zeitpunkten oder auch das interaktive Navigieren im Filmmaterial selbst betreffen. Obwohl derzeit noch niemand genau wei&szlig;, was die Zukunft hier bringen wird, m&ouml;chten wir bei dieser Entwicklung dabei sein.</p>
 
-<h2>Politische Implikationen</h2>
-<p>Aufgrund ihrer inhaltlichen Ausrichtung haben die StadtLandFluss-Geschichten auch eine zusätzliche kultur- und gesellschaftspolitische Dimension: Die Filme zeichnen ein authentisches Portrait der regionalen Lebenswelt im Neckar-Odenwald-Kreis und beziehen damit Position gegen eine sich überall ausbreitende "Entlokalisierung", Verflachung und Standardisierung von Kultur, die wir allenthalben - letztlich als Folge der Globalisierung - beobachten können. So betrachtet hat das Potential auch das Potential, eine Community-Bildung in der Region zu bewirken und zu fördern. Die Bewohner der Region sollen daher über ein begleitendes Social Media Projekt (auf Facebook) angesprochen werden. Interessant und wünschenswert wäre auch, relevante regionale Institutionen - von kulturellen Landesinstitutionen über die Stadt Mosbach bis hin zu lokalen Institutionen, Vereinen und Gewerbetreibenden - für eine Zusammenarbeit zu gewinnen.</p>
+		<h2>Politische Implikationen</h2>
+		<p>Aufgrund ihrer inhaltlichen Ausrichtung haben die StadtLandFluss-Geschichten auch eine zus&auml;tzliche kultur- und gesellschaftspolitische Dimension: Die Filme zeichnen ein authentisches Portrait der regionalen Lebenswelt im Neckar-Odenwald-Kreis und beziehen damit Position gegen eine sich &uuml;berall ausbreitende &bdquo;Entlokalisierung&ldquo;, Verflachung und Standardisierung von Kultur, die wir allenthalben &ndash; letztlich als Folge der Globalisierung &ndash; beobachten k&ouml;nnen. So betrachtet hat das Potential auch das Potential, eine Community-Bildung in der Region zu bewirken und zu f&ouml;rdern. Die Bewohner der Region sollen daher &uuml;ber ein begleitendes Social Media-Projekt (auf Facebook) angesprochen werden. Interessant und w&uuml;nschenswert w&auml;re auch, relevante regionale Institutionen &ndash; von kulturellen Landesinstitutionen &uuml;ber die Stadt Mosbach bis hin zu lokalen Institutionen, Vereinen und Gewerbetreibenden &ndash; f&uuml;r eine Zusammenarbeit zu gewinnen.</p>
 
-<h2>Danksagung / Ausblick</h2>
-<p>Das Projekt wurde in seiner Erstfassung im Sommer 2012 als Abschlussprojekt von zehn Studierenden des Jahrgangs 2009 erstellt, namentlich: Christian Albert, Julia Beck, Katharina Franz, Clara Gies, Sebastian Lauer, Rene Preußer, Rene Preisler, Lisa Simon und Elina Wetsch.</p>
+		<h2>Danksagung / Ausblick</h2>
+		<p>Das Projekt wurde in seiner Erstfassung im Sommer 2012 als Abschlussprojekt von zehn Studierenden des Jahrgangs 2009 erstellt, namentlich: Christian Albert, Julia Beck, Katharina Franz, Clara Gies, Sebastian Lauer, Rene Preu&szlig;er, Rene Preisler, Lisa Simon und Elina Wetsch.</p>
 
-<h2>Besonderen Dank schulden wir...</h2>
-<ul><li>...unseren Protagonisten Uli Körber, Siegfried Raether, Baron Dajo von Gemmingen-Hornberg, Serdar Batmaz, Brigitte Schmitt, Michaela Pusch, Werner Simon, Edgar Rotter und Alfred Ebner, den LKW-Fahrer,</li>
-<li>...unserem Komponisten Eckes Malz,</li>
-<li>...unseren Filmdozenten Daniela Michel und Alexander Kleider,</li>
-<li>...Carola Dziedzic, Markus Rollwa und Prof.Dr. Arnulf Mester vom Studiengang Onlinemedien</li>
-</ul>
-
-<p>Konzeptionell verantwortlicher Dozent und Initiator des Projekts ist Prof. Dr. Thomas Wirth vom Studiengang Onlinemedien. Er ist zugleich auch Ansprechpartner für alle Fragen und Anregungen rund um das Projekt.  Bei Fragen oder Anregungen schreiben Sie bitte an wirth@dhbw-mosbach.de</p>
+		<h2>Besonderen Dank schulden wir &hellip;</h2>
+		<ul>
+			<li>&hellip; unseren Protagonisten Uli K&ouml;rber, Siegfried Raether, Baron Dajo von Gemmingen-Hornberg, Serdar Batmaz, Brigitte Schmitt, Michaela Pusch, Werner Simon, Edgar Rotter und Alfred Ebner, den LKW-Fahrer,</li>
+			<li>&hellip; unserem Komponisten Eckes Malz,</li>
+			<li>&hellip; unseren Filmdozenten Daniela Michel und Alexander Kleider,</li>
+			<li>&hellip; Carola Dziedzic, Markus Rollwa und Prof. Dr. Arnulf Mester vom Studiengang Onlinemedien</li>
+		</ul>
+		
+		<p>Konzeptionell verantwortlicher Dozent und Initiator des Projekts ist Prof. Dr. Thomas Wirth vom Studiengang Onlinemedien. Er ist zugleich auch Ansprechpartner f&uuml;r alle Fragen und Anregungen rund um das Projekt. Bei Fragen oder Anregungen schreiben Sie bitte an <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#119;&#105;&#114;&#116;&#104;&#64;&#100;&#104;&#98;&#119;&#45;&#109;&#111;&#115;&#98;&#97;&#99;&#104;&#46;&#100;&#101;" rel="nofollow">&#119;&#105;&#114;&#116;&#104;&#64;&#100;&#104;&#98;&#119;&#45;&#109;&#111;&#115;&#98;&#97;&#99;&#104;&#46;&#100;&#101;</a>.</p>
 		</div>
 	</div>
 </div>

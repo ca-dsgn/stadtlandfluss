dbcc8c93762ade6eef4c007c3ab93a9a78a20ac9
Christian Albert <christian-albert@msn.com>
2012-07-03 11:46:15 +0200

    bugfix in player for ipad

--- js/main.js
+++ js/main.js
@@ -979,28 +979,9 @@ function new_video_player(video_src) {
 	
 	if ($("#player_container").length > 0) {
 		
-		if (swfobject.hasFlashPlayerVersion("8.0.0")) {
-			
-			var params = {	allowScriptAccess: "always",
-							movie: video_src,
-							wmode: "opaque",
-							"allowFullScreen": true
-			};
-			var atts = { id: "player" };
-			swfobject.embedSWF(video_src,
-							   "player_container", "800", "500", "8", null, null, params, atts);
-		}
-		else {
-			html = '<object id="player" data="' + video_src + '" style="width: 800px; height: 500px;">';
-			html+= '<param name="movie" value="' + video_src + '">';
-			html+= '<param name="wmode" value="opaque"/>';
-			html+= '<param name="allowFullScreen" value="true">';
-			html+= '<param name="allowScriptAccess" value="always">';
-			html+= '<embed style="width: 800px; height: 500px;" wmode="opaque" src="' + video_src + '" type="application/x-shockwave-flash" allowfullscreen="true" allowScriptAccess="always">';
-			html+= '</object>';
-			
-			$("#player_container").replaceWith(html);
-		}
+		html = '<embed id="player" style="width: 800px; height: 500px;" wmode="opaque" src="' + video_src + '" type="application/x-shockwave-flash" allowfullscreen="true" allowScriptAccess="always">';
+		
+		$("#player_container").replaceWith(html);
 	}
 }
 

155163ed3f63981c6a4d61e0c24815e3c67376b1
heartcrazed <katharina.franz@gmail.com>
2012-07-03 15:54:17 +0200

    neues file: voting

---
+++ voting.php
@@ -0,0 +1,20 @@
+﻿<?php 
+	include "parts/header.php";
+	include "parts/backButton.php";
+?>
+
+<div id="singlepageContent">
+	<div class="wrapper">
+		<div id="text">
+		
+		<h1>Voting</h1>
+		
+		</div>
+	</div>
+</div>
+
+
+<?php
+	include 'parts/footer.php';
+	
+?>

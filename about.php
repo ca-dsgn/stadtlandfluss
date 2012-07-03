8f6132447c2756e1510b1cb9ea9b21509cf45570
heartcrazed <katharina.franz@gmail.com>
2012-07-03 13:41:51 +0200

    motocross bilder getauscht auf wunsch von wirth, neuer about-text

--- about.php
+++ about.php
@@ -12,7 +12,26 @@
 		
 		<blockquote class="zitat">„Wie oft kommt es vor, dass ein Mensch eine interessante Facette hat, die niemand oder nur sehr wenige kennen?“</blockquote>
 		
-		<p id="last">Das Projekt <strong>StadtLandFluss-Geschichten</strong> will diese durch kleine Dokumentarfilme, welche seit 2004 vom <a href="http://dhbw-mosbach.de/on" target="_blank">Studiengang Onlinemedien</a> (früher: Digitale Medien) an der <a href="http://www.dhbw-mosbach.de" target="_blank">Dualen Hochschule Mosbach</a> in Kooperation mit der DOKWERK Filmkooperative produziert werden, zum Vorschein bringen und auf einer Projektwebseite zusammentragen. Mit dem Projekt sollen die Voraussetzungen geschaffen werden, die bisher relativ lose nebeneinander stehenden Filmclips zu einer Art „Gesamtkunstwerk“ zusammenzusetzen. In diesem Zusammenhang ist die Zeitperspektive wichtig: Es sollen über Jahre hinweg weitere Erzählungen gesammelt und dem Projekt dann immer weiter hinzugefügt werden.</p>
+		<h2>Die Entstehungsgeschichte</h2>
+<p>Das Projekt "StadtLandFluss-Geschichten" beruht auf einer Kooperation des Studiengangs Onlinemedien an der DHBW Mosbach und den Dokumentarfilmern der DOK-WERK Filmkooperative aus Köln. Diese Akteure stehen für die beiden wesentlichen Bestandteile des Projekts: Web-Technologie und Web-Design einerseits, und Film bzw. Dokumentarfilm andererseits. Das Projekt bildet einen gemeinsamen konzeptionellen Bezugsrahmen für die in der Lehre an der Hochschule im Rahmen des Curriculums "Audiovisuelle Gestaltung" immer wieder neu entstehenden Dokumentarfilme. Diese sollen nicht einfach gesammelt und archivert, sondern innerhalb eines wachsenden "Online-Filmwerks" zusammengesetzt und veröffentlicht werden.</p>
+
+<h2>Zielsetzung des Projekts innerhalb der Lehre</h2>
+<p>Die StadtLandFluss-Geschichten sollen es ermöglichen, in Zukunft auch mit den sich aktuell rasch entwickelnden technischen Möglichkeiten zu experimentieren, die das Web für die Gestaltung filmischer Erzählungen zur Verfügung stellt. Derzeit schon realisiert ist die eine interaktive Landkarte, auf der Episoden nach ihrer geografischen Ordnung angeordnet, gefunden und ausgewählt werden können und die Entwicklung einer grafischen Benutzeroberfläche für das Zusammenstellen von "Playlists". Komplexere Beispiele, die in Zukunft realisert werden sollen, könnten z.B. das Einblenden von Information außerhalb des primären Video-Streams zu bestimmten Zeitpunkten oder auch das interaktive Navigieren im Filmmaterial selbst betreffen. Obwohl derzeit noch niemand genau weiß, was die Zukunft hier bringen wird, möchten wir bei dieser Entwicklung dabei sein.</p>
+
+<h2>Politische Implikationen</h2>
+<p>Aufgrund ihrer inhaltlichen Ausrichtung haben die StadtLandFluss-Geschichten auch eine zusätzliche kultur- und gesellschaftspolitische Dimension: Die Filme zeichnen ein authentisches Portrait der regionalen Lebenswelt im Neckar-Odenwald-Kreis und beziehen damit Position gegen eine sich überall ausbreitende "Entlokalisierung", Verflachung und Standardisierung von Kultur, die wir allenthalben - letztlich als Folge der Globalisierung - beobachten können. So betrachtet hat das Potential auch das Potential, eine Community-Bildung in der Region zu bewirken und zu fördern. Die Bewohner der Region sollen daher über ein begleitendes Social Media Projekt (auf Facebook) angesprochen werden. Interessant und wünschenswert wäre auch, relevante regionale Institutionen - von kulturellen Landesinstitutionen über die Stadt Mosbach bis hin zu lokalen Institutionen, Vereinen und Gewerbetreibenden - für eine Zusammenarbeit zu gewinnen.</p>
+
+<h2>Danksagung / Ausblick</h2>
+<p>Das Projekt wurde in seiner Erstfassung im Sommer 2012 als Abschlussprojekt von zehn Studierenden des Jahrgangs 2009 erstellt, namentlich: Christian Albert, Julia Beck, Katharina Franz, Clara Gies, Sebastian Lauer, Rene Preußer, Rene Preisler, Lisa Simon und Elina Wetsch.</p>
+
+<h2>Besonderen Dank schulden wir...</h2>
+<ul><li>...unseren Protagonisten Uli Körber, Siegfried Raether, Baron Dajo von Gemmingen-Hornberg, Serdar Batmaz, Brigitte Schmitt, Michaela Pusch, Werner Simon, Edgar Rotter und Alfred Ebner, den LKW-Fahrer,</li>
+<li>...unserem Komponisten Eckes Malz,</li>
+<li>...unseren Filmdozenten Daniela Michel und Alexander Kleider,</li>
+<li>...Carola Dziedzic, Markus Rollwa und Prof.Dr. Arnulf Mester vom Studiengang Onlinemedien</li>
+</ul>
+
+<p>Konzeptionell verantwortlicher Dozent und Initiator des Projekts ist Prof. Dr. Thomas Wirth vom Studiengang Onlinemedien. Er ist zugleich auch Ansprechpartner für alle Fragen und Anregungen rund um das Projekt.  Bei Fragen oder Anregungen schreiben Sie bitte an wirth@dhbw-mosbach.de</p>
 		</div>
 	</div>
 </div>
